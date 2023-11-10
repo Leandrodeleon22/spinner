c9d815c7ff498c6560a8adeb4b108dfb5bdb5e42
leandrodeleon22 <leandrodeleon22@gmail.com>
2023-11-09 21:32:55 -0500

    spinner2

--- spinner2.js
+++ spinner2.js
@@ -9,28 +9,3 @@ animate.forEach((el) => {
   }, speed);
   speed += 200;
 });
-
-// setTimeout(() => {
-//   process.stdout.write("\r|   ");
-// }, 100);
-
-// setTimeout(() => {
-//   process.stdout.write("\r/   ");
-// }, 300);
-
-// setTimeout(() => {
-//   process.stdout.write("\r-   ");
-// }, 500);
-
-// setTimeout(() => {
-//   // Need to escape the backslash since it's a special character.
-//   process.stdout.write("\r\\   ");
-// }, 700);
-
-// setTimeout(() => {
-//   process.stdout.write("\r|   ");
-// }, 900);
-
-// setTimeout(() => {
-//   process.stdout.write("\r/   ");
-// }, 1100);
